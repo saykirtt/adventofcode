dfec5f24f7c4ff39750599a163c5e8ba0fee39f5
Saykirtt <r.pinon@hotmail.fr>
2022-12-10 08:41:10 +0100

    add 2022 9 part 2

--- src/2022/day09.py
+++ src/2022/day09.py
@@ -3,6 +3,9 @@ filename = 'inputs/2022/day09'
 with open(filename,'r') as file:
 	data = file.read().splitlines()
 
+rope_size = 10
+rope = [[0,0] for y in range(rope_size)]
+
 head = [0,0]
 tail = [0,0]
 
@@ -36,8 +39,9 @@ for row in data:
 	dir, step = row.split(' ')
 
 	for i in range(int(step)):
-		set_head_position(head,dir)
-		set_tail_position(tail,head)
-		positions.add(tuple(tail))
+		set_head_position(rope[0],dir)
+		for k in range(1,len(rope)):
+			set_tail_position(rope[k],rope[k-1])
+		positions.add(tuple(rope[rope_size-1]))
 
 print(len(positions))
