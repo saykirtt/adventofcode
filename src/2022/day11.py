5b4115f13a7e6083ee9adc7b811a4452bf06d117
Saykirtt <r.pinon@hotmail.fr>
2022-12-11 13:22:54 +0100

    refactor 2022 day 11 part 1 and add part 2

--- src/2022/day11.py
+++ src/2022/day11.py
@@ -1,5 +1,5 @@
 filename = 'inputs/2022/day11'
-filename = 'inputs/test'
+#filename = 'inputs/test'
 
 with open(filename,'r') as file:
 	data =file.read()
@@ -13,55 +13,41 @@ monkeys = []
 for m in monkeys_data:
 	rows = m.strip().splitlines()
 	monkey = {}
-	for r in rows:
-		key,value = r.split(':')
-		monkey[key.replace(" ","")]= value[1:].split(' ')
 
-	monkey['items'] = [int(i.replace(',','')) for i in monkey['Startingitems']]
+	monkey['items'] = list(map(int,rows[1].split(': ')[1].split(', ')))
+	monkey['operation'] = eval("lambda old:"+rows[2].split('=')[1])
+	monkey['divisible'] = int(rows[3].split(' ')[-1])
+	monkey['true'] = int(rows[4].split(' ')[-1])
+	monkey['false'] = int(rows[5].split(' ')[-1])
+
 	monkeys.append(monkey)
 
-#print(monkeys)
+monkey_score = [0] * len(monkeys)
 
-# dividible => a % b == 0
+#part 1
+#round = 20
+#part 2
+round = 10000
 
-def ceil(n):
-    return int(-1 * n // 1 * -1)
+mod = 1 # items over mod of all mod are not useful
+for monkey in monkeys:
+    mod *= monkey['divisible']
 
-def multiply(a,b):
-	#return ceil((a/10) * (b/10) *100)
-	return a * b 
-
-def addition(a,b):
-	return a+b
-
-sign = {
-	'*':multiply,
-	'+':addition
-}
-
-monkey_score = [0 for i in range(len(monkeys))]
-
-round = 20 #10000
 
 for r in range(round):
 	for m_no,m in enumerate(monkeys):
-		fct = sign[m['Operation'][3]]
-		value = m['Operation'][4]
-
-		divisible = int(m['Test'][2])
-
-		indexs = (int(m['Iftrue'][3]),int(m['Iffalse'][3]))
-
 		for i in m['items']:
-			b = i if value == 'old' else int(value)
-			new = fct(i,b) // 3
-			
-			if new % divisible == 0:
-				index = indexs[0]
-			else:
-				index = indexs[1]
+			#part 1
+			#new = m['operation'](i) //3S
+			#part 2
+			new = m['operation'](i)
+			new %= mod
 
-			monkeys[index]['items'].append(new)
+			if new % m['divisible'] == 0:
+				monkeys[m['true']]['items'].append(new)
+			else:
+				monkeys[m['false']]['items'].append(new)
+
 			monkey_score[m_no] += 1
 
 		m['items'] = []
@@ -69,4 +55,3 @@ print(monkey_score)
 monkey_score.sort()
 
 print(monkey_score[-1]*monkey_score[-2])
-
